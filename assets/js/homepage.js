9f4facb174ad959cb2437ad8779131ea141277a9
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-20 16:35:05 -0400

    added error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,12 @@ var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var displayRepos = function(repos, searchTerm) {
+    // check if API returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
     // clear old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
@@ -67,10 +73,18 @@ var getUserRepos = function (user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        console.log(response);
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos (data, user);
+            });
+        } else {
+            alert("Error: Github User Not Found.")
+        };
+    })
+    .catch(function(error) {
+        // notice this '.catch()' getting chained onto the end of the '.then()' method
+        alert("Unable to connect to Github.");
     });
 };
 
