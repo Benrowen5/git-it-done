3068a26b427624ec6a2db670e6796fd0a62ce805
Benrowen5 <79202800+Benrowen5@users.noreply.github.com>
2021-09-28 23:29:01 -0400

    added featured repos search based on language selection

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,20 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q="  + language + "is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub user not found');
+        }
+    });
+};
 
 var displayRepos = function(repos, searchTerm) {
     // check if API returned any repos
@@ -68,6 +82,16 @@ var formSubmitHandler = function(event) {
     }
 };
 
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
 var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -90,3 +114,4 @@ var getUserRepos = function (user) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonEl.addEventListener("click", buttonClickHandler);
